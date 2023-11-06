42ad6eb19be72d2310db66d28cb868bdad15a605
Felipe_Fit <83964268+Felipe-Fiti@users.noreply.github.com>
2023-11-06 10:18:49 -0300

    Update KeyBoard.java
    Coleção de caracteres

--- KeyBoard.java
+++ KeyBoard.java
@@ -6,7 +6,7 @@ import static java.lang.System.exit;
 public class KeyBoard implements KeyListener {
     private Cena cena;
     private Object exit;
-
+//espaço - 32 ENTER - 13
     public KeyBoard(Cena cena) {
         this.cena = cena;
     }
