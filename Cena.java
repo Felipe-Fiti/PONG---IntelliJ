8a94843efb1c653b686068754ea329309bb73588
Felipe_Fit <83964268+Felipe-Fiti@users.noreply.github.com>
2023-11-08 10:41:13 -0300

    Update Cena.java
    Adicionamento de pause e manutenção de menu

--- Cena.java
+++ Cena.java
@@ -10,7 +10,6 @@ import java.awt.Font;
 public class Cena implements GLEventListener{
     private float xMin, xMax, yMin, yMax, zMin, zMax;
     private TextRenderer textRenderer;
-    //dados da esfera
     public int placar = 0;
     public int vidas = 5;
     public int fase = 1;
@@ -20,63 +19,64 @@ public class Cena implements GLEventListener{
     public boolean fimJogo = false;
     public float ang;
     public int op;
-    //Preenchimento
     public int mode;
 
 
     @Override
-    public void init(GLAutoDrawable drawable) {
-        //dados iniciais da cena
+    public void init(GLAutoDrawable drawable){
         GL2 gl = drawable.getGL().getGL2();
-        //Estabelece as coordenadas do SRU (Sistema de Referencia do Universo)
+        //SRU
         xMin = yMin = zMin = -100;
         xMax = yMax = zMax = 100;
 
         reset();
 
         textRenderer = new TextRenderer(new Font("Comic Sans MS Negrito", Font.BOLD, 20));
-        //Habilita o buffer de profundidade
+        //Profundidade
         gl.glEnable(GL2.GL_DEPTH_TEST);
     }
 
     public void reset(){
-
-        //dados da esfera
         placar = 0;
         vidas = 5;
-        fase = 15;
-
-        //preenchimento
+        fase = 1;
         mode = GL2.GL_FILL;
     }
 
+    public void continuarJogo(){
+        placar = 0;
+        vidas = 5;
+        fase = 1;
+    }
+
     @Override
-    public void display(GLAutoDrawable drawable) {
+    public void display(GLAutoDrawable drawable){
         //obtem o contexto Opengl
         GL2 gl = drawable.getGL().getGL2();
-        //objeto para desenho 3D
         GLUT glut = new GLUT();
-
-        //define a cor da janela (R, G, G, alpha)
+        //cor da janela - Branco
         gl.glClearColor(1, 1, 1, 1);
-        //limpa a janela com a cor especificada
         gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
-        gl.glLoadIdentity(); //ler a matriz identidade
+        gl.glLoadIdentity();
 
+        if(Menu){
+            desenhaTexto(gl, 730, 690, Color.BLACK, "Seja bem-vindo ao nosso jogo - PONG!");
+            desenhaTexto(gl, 880, 650, Color.BLACK, "REGRAS:");
+            desenhaTexto(gl, 400, 620, Color.BLACK, "Para jogar use as teclas da seta esquerda (para andar a esquerda) e a seta direita (para andar a direita) do teclado!");
+            desenhaTexto(gl, 760, 590, Color.BLACK, "Para PAUSAR o jogo aperte a tecla p!");
+            desenhaTexto(gl, 730, 559, Color.BLACK, "Para começar o jogo aperte a tecla ENTER!");
+            desenhaTexto(gl, 760, 530, Color.BLACK, "Para sair do jogo aperte a tecla ESC!");
 
-        //Modo de desenho - os parametros s�o constantes inteiras
-        //int modo =  GL2.GL_FILL; ou GL2.GL_LINE ou GL2.GL_POINT
-        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, mode);
+        }else if(jogo){
+            desenhaTexto(gl, 10, 1060, Color.BLACK, "Jogo: PONG");
+            desenhaTexto(gl, 10, 1035, Color.BLACK, "Fase: " + fase);
+            desenhaTexto(gl, 10, 1010, Color.BLACK, "Placar: " + placar);
+            desenhaTexto(gl, 10, 985, Color.BLACK, "Vidas: " + vidas);
 
-        desenhaTexto(gl, 10, 1060, Color.BLACK, "Jogo: PONG");
-        desenhaTexto(gl, 10, 1040, Color.BLACK, "Placar: " + placar);
-        desenhaTexto(gl, 10, 1020, Color.BLACK, "Vidas: " + vidas);
-
-        desenhaTexto(gl, 730, 690, Color.BLACK ,"Seja bem-vindo ao nosso jogo - PONG!");
-        desenhaTexto(gl, 880, 650, Color.BLACK ,"REGRAS:");
-        desenhaTexto(gl, 600, 620, Color.BLACK ,"Para jogar use somente as teclas da seta esquerda e direita do teclado!");
-        desenhaTexto(gl, 760, 590, Color.BLACK ,"Para PAUSAR o jogo aperte a tecla p!");
-        desenhaTexto(gl, 775, 558, Color.BLACK ,"Para começar o jogo aperte ENTER!");
+        }else if(pause){
+            desenhaTexto(gl, 800, 700, Color.BLACK, "O jogo está Pausado!");
+            desenhaTexto(gl, 710, 650, Color.BLACK, "Aperte a letra p para continuar o jogo!");
+        }
 
         gl.glFlush();
     }
@@ -92,7 +92,7 @@ public class Cena implements GLEventListener{
     }
 
     @Override
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height){
         //obtem o contexto grafico Opengl
         GL2 gl = drawable.getGL().getGL2();
 
@@ -109,5 +109,5 @@ public class Cena implements GLEventListener{
         System.out.println("Reshape: " + width + ", " + height);
     }
     @Override
-    public void dispose(GLAutoDrawable drawable) {}
+    public void dispose(GLAutoDrawable drawable){}
 }
