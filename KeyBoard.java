c5acc033570c1e6cc5d8e437171ad7bcfc4faeef
Felipe_Fit <83964268+Felipe-Fiti@users.noreply.github.com>
2023-11-08 10:42:22 -0300

    Update KeyBoard.java
    Adicionamento das teclas e funcionamento

--- KeyBoard.java
+++ KeyBoard.java
@@ -3,35 +3,33 @@ import com.jogamp.newt.event.KeyListener;
 
 import static java.lang.System.exit;
 
-public class KeyBoard implements KeyListener {
+public class KeyBoard implements KeyListener{
     private Cena cena;
     private Object exit;
-//espaço - 32 ENTER - 13
-    public KeyBoard(Cena cena) {
+    //espaço - 32 ENTER - 13
+    public KeyBoard(Cena cena){
         this.cena = cena;
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(KeyEvent e){
         System.out.println("Key pressed: " + e.getKeyCode());
         if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
             exit(0);
 
-        switch (e.getKeyChar()) {
-            case 'r':
-                System.out.println("chamou Rotacao");
-                cena.ang += 45.0f;
+        switch (e.getKeyChar()){
+            case 80://TECLA P - Pause/Stop do jogo
+                cena.pause =! cena.pause;
+                cena.Menu = false;
+                cena.continuarJogo();
                 break;
-
-            case 'p':
-                System.out.println("chamou Pause");
-                break;
-
-            case '1':
-                cena.op = 1;
+            case 13://Tecla ENTER - Start Jogo
+                cena.jogo =! cena.jogo;
+                cena.Menu = false;
+                cena.pause =! cena.jogo;
                 break;
         }
     }
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e){}
 }
